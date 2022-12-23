a3f024e8549fa6d415c44268657d5290bd5b7b2d
Guna Anban G <anbangisak@gmail.com>
2022-12-23 17:25:20 +0530

    arrVsSlice added

---
+++ arrVsSlice.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := [3]string{"hi", "hai", "spoke"}
+	fmt.Println(arr)
+	fmt.Println("slicing value: ", arr[1:3])
+}
