ed747bf17114a028b0a1f157cab7c7734e0f8796
Guna Anban G <anbangisak@gmail.com>
2022-12-23 16:20:55 +0530

    printUtcCode added

---
+++ printUtcTime.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	fmt.Println(time.Now().UTC())
+}
