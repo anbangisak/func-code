dd7ee9579b0ea0ae9e2bc89a3cdf5cef723d0aca
Guna Anban G <anbangisak@gmail.com>
2022-12-23 16:43:27 +0530

    enum values declared

---
+++ enuDec.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	enu = iota
+	enu1
+	enu2
+)
+
+func main() {
+	fmt.Println(enu, enu1, enu2)
+}
