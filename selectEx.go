22e28ce06baeef5a52e52fd07c1ff5c39da0b137
Guna Anban G <anbangisak@gmail.com>
2022-12-26 20:00:18 +0530

    select channel data catch

---
+++ selectEx.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func disp(ch chan string, strArr []string, dur int) {
+	defer wg.Done()
+	for _, str := range strArr {
+		time.Sleep(time.Duration(dur) * time.Millisecond)
+		ch <- str
+	}
+}
+
+func main() {
+	fmt.Println()
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	strArr1 := [3]string{"hi", "hai", "fresh"}
+	strArr2 := [3]string{"mi", "mai", "mresh"}
+	wg.Add(1)
+	go disp(ch1, strArr1[:], 10)
+	wg.Add(1)
+	go disp(ch2, strArr2[:], 20)
+
+	for i := 0; i < len(strArr1)+len(strArr2); i++ {
+		time.Sleep(time.Duration(100) * time.Millisecond)
+		select {
+		case val := <-ch1:
+			fmt.Println("ch1: ", val)
+		case val2 := <-ch2:
+			fmt.Println("ch2: ", val2)
+		}
+	}
+}
