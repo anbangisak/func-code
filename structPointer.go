cb7e3daa3ce36fea8d22a6f482697891f3fc2682
Guna Anban G <anbangisak@gmail.com>
2022-12-26 21:20:29 +0530

    value/pointer receiver added

--- structPointer.go
+++ structPointer.go
@@ -12,8 +12,24 @@ func (f *Fish) netMeter() {
 	fmt.Println(f.Water)
 }
 
+func valueReceiver(f Fish) {
+	f.Net = 20
+	fmt.Println("Inside ValueReceiver : ", f.Net)
+}
+func pointerReceiver(f *Fish) {
+	f.Water = 24
+	fmt.Println("Inside PointerReceiver: ", f.Water)
+}
+
 func main() {
 	fmt.Println()
+	f1 := Fish{11, 28}
+	f2 := &Fish{12, 68}
+
+	valueReceiver(f1)
+	fmt.Println("Outside valueRec: ", f1.Net)
+	pointerReceiver(f2)
+	fmt.Println("Inside PointerRec: ", f2.Water)
 	f := Fish{
 		Net:   10,
 		Water: 10,
